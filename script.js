c9ba7ced9d06be95c2b0da249ef95de04e6672f8
heguanelvis <heguanelvis@gmail.com>
2019-06-03 20:56:31 -0500

    cleaned data, added map and refactored basic styles

--- script.js
+++ script.js
@@ -18,8 +18,14 @@ const projection = d3
 const geoGenerator = d3.geoPath(projection);
 
 d3.json("data/us_states.json").then(data => {
-  console.log(data);
-  graph
+  console.log(d3.extent(data.features, d => d.properties.solar_electricity));
+
+  elecThresHold = d3
+    .scaleThreshold()
+    .domain([0, 5, 50, 1000, 18000, 30000])
+    .range(["#ffffff", "#fffac6", "#fff486", "#fcee21", "#f9c524"]);
+
+  mapPath = graph
     .selectAll("path")
     .data(data.features)
     .enter()
@@ -27,5 +33,36 @@ d3.json("data/us_states.json").then(data => {
     .attr("d", geoGenerator)
     .style("stroke", "black")
     .style("stroke-width", 2)
-    .attr("fill", "red");
+    .attr("fill", d => elecThresHold(d.properties.solar_electricity));
+
+  mapPath
+    .on("mouseover", handleMouseOver.bind(this))
+    .on("mouseout", handleMouseOut.bind(this));
 });
+
+function handleMouseOver(d, i, n) {
+  const centroid = geoGenerator.centroid(d);
+
+  d3.select(n[i]).attr("fill", "white");
+
+  tooltip = graph
+    .append("foreignObject")
+    .attr("width", 120)
+    .attr("height", 54)
+    .attr("id", `t-${d.properties.land_area_sq_km}-${i}`)
+    .attr("x", centroid[0])
+    .attr("y", centroid[1] - 80)
+    .html(() => {
+      let content = `<div class="tip-style"><div>${d.properties.state}</div>`;
+      content += `<div>${d.properties.solar_electricity}</div></div>`;
+      return content;
+    });
+}
+
+function handleMouseOut(d, i, n) {
+  d3.select(n[i]).attr("fill", d =>
+    elecThresHold(d.properties.solar_electricity)
+  );
+
+  d3.select(`#t-${d.properties.land_area_sq_km}-${i}`).remove();
+}
